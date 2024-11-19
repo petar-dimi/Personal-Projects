ac29c6e2970d54c554ec376b3617e94d6825e0da
Petar Dimiskovksi <dimiskovskipetar@yahoo.com>
2024-11-19 16:18:01 +0100

    Added transactions

--- Vue Tracker/src/components/Transactions.vue
+++ Vue Tracker/src/components/Transactions.vue
@@ -1,11 +1,25 @@
 <template>
     <h3>History</h3>
       <ul id="list" class="list">
-         <!-- <li class="minus">
-          Cashout <span>-$400</span><button class="delete-btn">x</button>
-        </li> 
-           <li class="plus">
-          Deposit <span>+$800</span><button class="delete-btn">x</button>
-        </li>  -->
+        <li v-for="transaction in transactions ":key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+
+          {{ transaction.text }} <span>-${{ transaction.amount  }}</span><button class="delete-btn">x</button>
+
+        </li>
+
       </ul>
 </template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  transactions:{
+    type: Array,
+    Required: true,
+  }
+});
+</script>
+
+
+
